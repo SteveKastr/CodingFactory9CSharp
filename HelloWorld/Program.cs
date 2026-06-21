5ab83a51feb2e95a193279087b7c3f6fbc0ac158
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-06-22 01:34:07 +0300

    chore: add comments and exe

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
+    /// <summary>
+    /// This is a simple C# program that prints "Hello, World!" to the console.
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine();
         }
     }
 }
