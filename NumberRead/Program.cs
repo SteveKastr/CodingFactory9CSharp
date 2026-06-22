aba73fbe6ac1a28b92d1f30352de4c0a116d3c6e
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-06-22 20:04:58 +0300

    feat: add simple number example

---
+++ NumberRead/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NumberRead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float floatNum = 0F;
+            double doubleNum = 0D;
+
+            Console.WriteLine("Εισάγετε δύο δεκαδικούς:");
+
+            if (!float.TryParse(Console.ReadLine(), out floatNum))
+            {
+                Console.WriteLine("Μη έγκυρη είσοδος για float.");
+                return;
+            }
+
+            if (!double.TryParse(Console.ReadLine(), out doubleNum))
+            {
+                Console.WriteLine("Μη έγκυρη είσοδος για double.");
+                return;
+            }
+
+            Console.WriteLine($"float = {floatNum,-6:N2}\t double = {doubleNum, -10:N2}");
+        }
+    }
+    
+}
