0cfdea8822a8efed0e34c3ecd534e34d64089f7c
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-06-24 22:03:25 +0300

    feat: add account example

---
+++ AccountApp/Exceptions/InsufficientBalanceException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountApp.Exceptions
+{
+    internal class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException()
+            : base("Insufficient balance")
+        { }
+
+        public InsufficientBalanceException(string message)
+            : base(message) { }
+
+        public InsufficientBalanceException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
